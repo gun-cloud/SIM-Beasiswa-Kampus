d8f622ef69d677546585eb7ea6a5f245f5bba4bd
Hendri Gunawan <hendri.gunawan2604@gmail.com>
2023-10-10 09:28:46 +0700

    membuat halaman beasiswa dan controller beasiswa

--- app/Http/Controllers/BeasiswaController.php
+++ app/Http/Controllers/BeasiswaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Beasiswa;
 use Illuminate\Http\Request;
 
 class BeasiswaController extends Controller
@@ -13,7 +14,10 @@ class BeasiswaController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.beasiswa',[
+            'title' => 'Beasiswa',
+            'beasiswas' => Beasiswa::all(),
+        ]);
     }
 
     /**
---
+++ resources/views/admin/beasiswa.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content-wrapper">
+    <div class="card">
+        @if($message=Session::get('success'))
+        <div class="alert alert-success">
+            {{$message}}
+        </div>
+        @endif
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Error!</strong> <br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <div class="card-body d-flex justify-content-between align-items-center">
+            <h4 class="card-title">Data Beasiswa</h4>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#tambah">
+                Tambah
+            </button>
+            {{-- @include('admin.jurusan-tambah') --}}
+        </div>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Jurusan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($beasiswas as $index => $beasiswa)
+                    <tr>
+                        <td>{{$index+1}}</td>
+                        <td>{{$beasiswa->nama}}</td>
+                        <td>
+                            {{-- @include('admin.jurusan-ubah') --}}
+                            <form action="{{route('jurusan.destroy',$beasiswa->id)}}" method="post">
+                                <button type="button" class="badge badge-warning" data-toggle="modal" data-target="#ubah{{$beasiswa->id}}">
+                                    Ubah
+                                </button>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="badge badge-danger" data-toggle="modal" data-target="#hapus">
+                                    Hapus
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="3" class="text-center"><i>Data Kosong</i></td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
