4a17d27a4c1b90f2e859a34055cea4d3d97d6345
unknown <ikefadillah112@gmail.com>
2023-10-12 08:15:29 +0700

    login admin

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomAuthController extends Controller
+{
+    public function index()
+    {
+        return view('loginUser');
+    }  
+       
+ 
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+    
+        $credentials = $request->only('email', 'password'); //credentials 
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('Signed in');
+        }
+   
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+    public function registration()
+    {
+        return view('registration');
+    }
+       
+ 
+    public function customRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+            
+        $data = $request->all();
+        $check = $this->create($data);
+          
+        return redirect("admin.dashboard")->withSuccess('have signed-in');
+    }
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }    
+     
+ 
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+   
+        return redirect("login")->withSuccess('are not allowed to access');
+    }
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+   
+        return Redirect('login');
+    }
+}
