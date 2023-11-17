8aaea1ce55c528f63e0fcd4ba2d937158d1b57f1
Hendri Gunawan <hendri.gunawan2604@gmail.com>
2023-11-17 18:30:17 +0700

    membuat form login admin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,16 +8,13 @@
     <title>Beasiswa Polmed | Login </title>
 </head>
 <body>
-    @error('password')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-    @enderror
-    @error('nim')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-    @enderror
+    <div class="col-md-6 offset-md-3">
+        @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+        @endif
+    </div>
     <div class="container-scroller">
         <div class="container-fluid page-body-wrapper full-page-wrapper">
             <div class="content-wrapper d-flex align-items-center auth px-0">
@@ -27,12 +24,12 @@
                             <div class="brand-logo">
                                 <img src="../../images/logo-polmed.svg" alt="logo">
                             </div>
-                            <h4>Hello! let's get started</h4>
+                            <h4>Hello! Admin, let's get started</h4>
                             <h6 class="font-weight-light">Sign in to continue.</h6>
-                            <form action="" method="POST" class="pt-3">
+                            <form action="{{route('login.proses')}}" method="POST" class="pt-3">
                                 @csrf
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-lg" name="nim" placeholder="NIM">
+                                    <input type="text" class="form-control form-control-lg" name="email" placeholder="Email">
                                 </div>
                                 <div class="form-group">
                                     <input type="password" class="form-control form-control-lg" name="password" placeholder="Password">
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\BeasiswaController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +21,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/login', [HomeController::class, 'login'])->name('login');
+// Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::resource('/jurusan', JurusanController::class);
-Route::resource('/prodi', ProdiController::class);
-Route::resource('/mahasiswa', MahasiswaController::class);
-Route::resource('/beasiswa', BeasiswaController::class);
+// Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+// Route::post('/login', [LoginController::class, 'login'])->name('login.proses')->middleware('guest');
+// Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+// Route::resource('/jurusan', JurusanController::class);
+// Route::resource('/prodi', ProdiController::class);
+// Route::resource('/mahasiswa', MahasiswaController::class);
+// Route::resource('/beasiswa', BeasiswaController::class);
+Route::get('/', [HomeController::class, 'index']);
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'login'])->name('login.proses');
+});
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::resource('/jurusan', JurusanController::class);
+    Route::resource('/prodi', ProdiController::class);
+    Route::resource('/mahasiswa', MahasiswaController::class);
+    Route::resource('/beasiswa', BeasiswaController::class);
+});
